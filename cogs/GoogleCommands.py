3568695c8c73ffb4e10c9d1cdf95d875e85c0d4b
Nuno Antunes <nuno.f.antunes@inov.pt>
2021-07-13 10:54:08 +0100

    removed old googleapi module

--- cogs/GoogleCommands.py
+++ cogs/GoogleCommands.py
@@ -1,5 +1,4 @@
-from googleapi import google
-from googleapi import google, images
+# from googleapi import google, images
 
 import discord
 from discord.ext import commands
@@ -13,29 +12,29 @@ class GoogleCommands(commands.Cog):
     Commands
     '''
     
-    @commands.command(name='google')
-    async def google_search_command(self, ctx, input_query):
-        search_results = google.search(input_query)
-        await ctx.channel.send("This is going to be the result of the search!")
+    # @commands.command(name='google')
+    # async def google_search_command(self, ctx, input_query):
+    #     search_results = google.search(input_query)
+    #     await ctx.channel.send("This is going to be the result of the search!")
         
-    @commands.command(name='calculate')
-    async def calculate_command(self, ctx, input_query):
-        search_results = google.search(input_query)
-        await ctx.channel.send(search_results)
+    # @commands.command(name='calculate')
+    # async def calculate_command(self, ctx, input_query):
+    #     search_results = google.search(input_query)
+    #     await ctx.channel.send(search_results)
         
-    @commands.command(name='img')
-    async def img_command(self, ctx, input_query):
-        options = images.ImageOptions()
-        #options.image_type = images.ImageType.CLIPART
-        #options.larger_than = images.LargerThan.MP_4
-        #options.color = "green"
-        results = google.search_images(input_query, options)
-        await ctx.channel.send(results)
+    # @commands.command(name='img')
+    # async def img_command(self, ctx, input_query):
+    #     options = images.ImageOptions()
+    #     #options.image_type = images.ImageType.CLIPART
+    #     #options.larger_than = images.LargerThan.MP_4
+    #     #options.color = "green"
+    #     results = google.search_images(input_query, options)
+    #     await ctx.channel.send(results)
      
-    @commands.command(name='convert')
-    async def convert_command(self, ctx, input_query):
-        euros = google.convert_currency(5.0, "USD", "EUR")
-        await ctx.channel.send("5.0 USD = {0} EUR".format(euros))
+    # @commands.command(name='convert')
+    # async def convert_command(self, ctx, input_query):
+    #     euros = google.convert_currency(5.0, "USD", "EUR")
+    #     await ctx.channel.send("5.0 USD = {0} EUR".format(euros))
         
     @commands.command(name='youtube', aliases=['yt'])
     async def youtube_search(self, ctx, *input_query):
